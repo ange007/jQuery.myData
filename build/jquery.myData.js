6dfccbfc557637b4fc04e997a5479fff99378f8a
Vladimir B <ange305@gmail.com>
2019-05-25 12:43:54 +0300

    New GH-Pages

--- build/jquery.myData.js
+++ build/jquery.myData.js
@@ -1,6 +1,6 @@
 /**
  * jquery.mydata - Small JQuery&Zepto plugin for two-ways data binding.
- * @version v0.2.1
+ * @version v0.4.2
  * @link https://github.com/ange007/JQuery.myData
  * @license MIT
  * @author Borisenko Vladimir
@@ -43,7 +43,8 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 		this.keys = {
 			'event': 'data-on',
 			'event-value': 'data-on-value',
-			'data': 'data-bind'
+			'data': 'data-bind',
+			'data-element': 'data-bind-element'
 		};
 
 		// Event and Data target
@@ -56,7 +57,8 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 				this.keys = {
 					'event': targetOrOptions['data-keys']['event'] || this.keys['event'],
 					'event-value': targetOrOptions['data-keys']['event-value'] || this.keys['event-value'],
-					'data': targetOrOptions['data-keys']['data'] || this.keys['data']
+					'data': targetOrOptions['data-keys']['data'] || this.keys['data'],
+					'data-element': targetOrOptions['data-keys']['data-element'] || this.keys['data-element']
 				};
 			}
 		} else {
@@ -79,7 +81,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 	};
 
 	Plugin.prototype = {
-		// Навешивание событий и обработчиков
+		// Add events
 		bind: function bind() {
 			var context = this;
 
@@ -94,7 +96,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 					return;
 				}
 
-				// Записываем элемент
+				// Add element to list
 				context.bindings.push({ element: item, property: propName, value: undefined });
 			});
 
@@ -102,33 +104,35 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 			this._setCheckTimer();
 		},
 
-		// Снятие событий и обработчиков
+		// Remove events
 		unbind: function unbind() {
 			// Отключение проверки событий
-			this.element.off('.' + pluginName, '[' + this.keys['data'] + ']').off('.' + pluginName, '[' + this.keys['event'] + ']').off('.' + pluginName, '[' + this.keys['event-value'] + ']');
+			this.element.off('.' + pluginName, '[' + this.keys['data'] + ']').off('.' + pluginName, '[' + this.keys['data-element'] + ']').off('.' + pluginName, '[' + this.keys['event'] + ']');
 
-			// Таймер проверки значений
+			// Clear timer
 			clearInterval(this.checkTimer);
 
 			//
 			this.bindings = [];
 		},
 
-		// Реакция на изменение состояния элемента
+		// Set events 
 		_setEventListeners: function _setEventListeners() {
 			var context = this;
-			var bindEvents = ['change', 'keydown', 'input', 'paste'].map(function (item) {
+			var bindEvents = ['change', 'keyup', 'input', 'paste'].map(function (item) {
 				return item += '.' + pluginName;
 			});
 			var onEvents = ['click', 'dblclick', 'change', 'input', 'paste', 'load', 'unload', 'select', 'resize', 'scroll', 'submit', 'error', 'keydown', 'keyup', 'keypress', 'mouseover', 'mousedown', 'mouseup', 'mousemove', 'mouseout', 'mouseenter', 'mouseleave', 'blur', 'focus', 'focusin', 'focusout'].map(function (item) {
 				return item += '.' + pluginName;
 			});
 
-			// Реакция на смену состояния элемента
+			// Change state reaction
 			// data-bind="key" 
 			this.element.on(bindEvents.join(' '), '[' + this.keys['data'] + ']', function (event) {
 				var element = $(event.target);
 				var targetKey = element.attr(context.keys['data']);
+
+				//
 				var value = undefined;
 
 				// Заменяем значение в список
@@ -139,17 +143,17 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 						continue;
 					}
 
-					// Считываем значение
+					// Read value
 					value = context._readElementValue(element, item.value);
 
-					// Имя функции для установки значения
+					// Generate function name
 					var setFunctionName = 'set' + targetKey.charAt(0).toUpperCase() + targetKey.substr(1);
 
-					// Если значение изменилось с прошлого раза
+					// Only if value changed
 					if (value !== item.value) {
 						item.value = value;
 
-						// Установка значения
+						// Send value
 						if (typeof context.dataTarget[setFunctionName] === 'function') {
 							context.dataTarget[setFunctionName].apply(context.dataTarget, [value]);
 						} else if (typeof context.dataTarget[targetKey] === 'function') {
@@ -164,116 +168,121 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 				//
 				if (typeof context.callbacks.set === 'function') {
-					context.callbacks.set(element, targetKey, value);
+					context.callbacks.set(element, targetKey, value, { type: event.type });
 				} else if (typeof context.callbacks.main === 'function') {
-					context.callbacks.main('set', element, targetKey, value);
+					context.callbacks.main('set', element, targetKey, value, { type: event.type });
 				}
 			});
 
-			// Обработка установленных событий
-			// data-on="focusin,focusout:action" 
+			// Element connection
+			// data-bind-element="enabled,text:#id"
+			// data-bind-element="[enabled:#id,text:input[name=qqq]]"
+			// data-bind-element="visible:.class"
+			this.element.on(bindEvents.join(' '), '[' + this.keys['data-element'] + ']', function (event) {
+				var element = $(event.target);
+				var actionData = element.attr(context.keys['data-element']);
+
+				context._extractActions(element, event, actionData, function (action, selector) {
+					var targetElement = $(selector);
+
+					// Read value
+					var value = context._readElementValue(element, undefined);
+
+					// Set value
+					context._setElementValue(targetElement, action, value);
+				});
+			});
+
+			// Action reaction
+			// data-on="focusin,focusout:action"
 			// data-on="[click:action1,change:action2]"
 			// data-on="click:test( '!!!' )"
 			this.element.on(onEvents.join(' '), '[' + this.keys['event'] + ']', function (event) {
 				var element = $(this);
 				var actionData = element.attr(context.keys['event']);
 
-				//
-				var actionList = [];
-
-				//
-				if (typeof actionData !== 'string') {
-					console.error('jQuery.myData: Empty data in [' + context.keys['event'] + '].');
-					return;
-				} else {
-					actionList = actionData.indexOf('[') === 0 ? (actionData.match(/[\[\{}](.*?)[\}\]]/)[1] || '').split(',') : [actionData];
-				}
-
-				//
-				actionList.forEach(function (action, i, arr) {
-					var onData = action.indexOf(':') >= 0 ? action.split(':') : ['click', action];
-					var eventTypes = onData[0].trim().split(',');
-					var handlerName = onData[1].trim();
+				context._extractActions(element, event, actionData, function (eventType, handlerName) {
 					var handlerFunc = handlerName.match(/([a-zA-Z0-9,\.\-_\/]+)(?:\(([^)]+)\))?$/) || false;
 
-					// Если данной событие не указано в перечне - игнорируем
-					if (eventTypes.indexOf(event.type) < 0) {
+					//
+					if (eventType != event.type || !handlerFunc) {
 						return;
 					}
 
-					// Вызов функции
-					if (handlerFunc) {
-						var name = handlerFunc[1];
-						var args = typeof handlerFunc[2] === 'string' ? handlerFunc[2].split(',').map(function (item) {
-							return item.trim().match(/^['"]{0,}(.*?)['"]{0,}$/)[1] || '';
-						}) : [];
-						var targetFuncExist = context.eventTarget !== undefined && typeof context.eventTarget[name] === 'function';
-						var windowFuncExist = typeof window[name] === 'function';
+					//
+					var name = handlerFunc[1];
+					var args = typeof handlerFunc[2] === 'string' ? handlerFunc[2].split(',').map(function (item) {
+						return item.trim().match(/^['"]{0,}(.*?)['"]{0,}$/)[1] || '';
+					}) : [];
+					var targetFuncExist = context.eventTarget !== undefined && typeof context.eventTarget[name] === 'function';
+					var windowFuncExist = typeof window[name] === 'function';
 
-						// Считываем значение элемента
-						var value = context._readElementValue(element, undefined);
-						// const callArgs = ( element.is( '[' + context.keys[ 'event-value' ] + ']' ) ? args.concat( [ element, value ] ) : ( args || [ element, value ] ) );
-						var callArgs = [element, value].concat([args, event]);
+					// Read value
+					var value = context._readElementValue(element, undefined);
 
-						//
-						var result = undefined;
+					//
+					var callArgs = $.extend([], args);
+					if (callArgs.length > 0) {
+						callArgs.push([element, event, value]);
+					} else {
+						callArgs = [value, [element, event, value]];
+					}
 
-						// Вызываем функцию
-						if (targetFuncExist) {
-							result = context.eventTarget[name].apply(context.eventTarget, callArgs);
-						} else {
-							try {
-								result = eval(name).apply(window, callArgs);
-							} catch (err) {
-								console.warn('jQuery.myData: Could not call - "' + name + '"');
-							}
-						}
+					//
+					var result = undefined;
 
-						// Вызываем события
-						if (typeof context.callbacks.on === 'function') {
-							context.callbacks.on(element, event.type, value);
-						} else if (typeof context.callbacks.main === 'function') {
-							context.callbacks.main('on', element, event.type, value);
-						}
-
-						//
-						if (result === false) {
-							event.stopPropagation();
-
-							return false;
-						}
-						// 
-						else result === true;
-						{
-							event.preventDefault();
-
-							return true;
+					// Call function
+					if (targetFuncExist) {
+						result = context.eventTarget[name].apply(context.eventTarget, callArgs);
+					} else {
+						try {
+							result = eval(name).apply(window, callArgs);
+						} catch (err) {
+							console.warn('jQuery.myData: Could not call - "' + name + '"');
 						}
 					}
+
+					// Callback
+					if (typeof context.callbacks.on === 'function') {
+						context.callbacks.on(element, name, value, { type: event.type, args: args });
+					} else if (typeof context.callbacks.main === 'function') {
+						context.callbacks.main('on', element, name, value, { type: event.type, args: args });
+					}
+
+					//
+					if (result === false) {
+						event.stopPropagation();
+
+						return false;
+					}
+					//
+					else if (result === true) {
+							event.preventDefault();
+
+							return result;
+						}
 				});
 			});
 		},
 
-		// Таймер прослушивания изменений объекта
+		// Timer 
 		_setCheckTimer: function _setCheckTimer() {
 			var delay = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 250;
 
 			var context = this;
 
-			// Таймер проверки значений
 			this.checkTimer = setInterval(function () {
 				for (var i in context.bindings) {
-					// Изменяемая ссылка на текущий элемент
 					var item = context.bindings[i];
 					var element = $(item.element);
 					var targetKey = item.property;
 					var oldValue = item.value;
 					var value = '';
 
-					// Имя функции для установки значения
+					// Generate function name
 					var getFunctionName = 'get' + targetKey.charAt(0).toUpperCase() + targetKey.substr(1);
 
-					// Проверка функции "get"
+					// Read value
 					if (typeof context.dataTarget[getFunctionName] === 'function') {
 						value = context.dataTarget[getFunctionName]();
 					} else if (typeof context.dataTarget[targetKey] === 'function') {
@@ -282,82 +291,169 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 						value = context.dataTarget[targetKey];
 					}
 
-					// Меняем значение элемента
-					if (value !== oldValue) {
-						item.value = value;
+					// Only if value changed
+					if (value === oldValue) {
+						continue;
+					}
+					item.value = value;
 
-						//
-						if (element.is('input[type="checkbox"]') || element.is('input[type="radio"]')) {
-							$(element).attr('checked', value);
-						} else if (element.is('select') || element.is('input') || element.is('textarea')) {
-							$(element).val(value);
-						} else {
-							$(element).html(value);
-						}
+					// Set value to element
+					if (element.is('input[type="checkbox"]') || element.is('input[type="radio"]')) {
+						$(element).attr('checked', value);
+					} else if (element.is('select') || element.is('input') || element.is('textarea')) {
+						$(element).val(value);
+					} else {
+						$(element).html(value);
+					}
 
-						//
-						if (typeof context.callbacks.get === 'function') {
-							context.callbacks.get(element, targetKey, value);
-						} else if (typeof context.callbacks.main === 'function') {
-							context.callbacks.main('get', element, targetKey, value);
-						}
+					// Callback
+					if (typeof context.callbacks.get === 'function') {
+						context.callbacks.get(element, targetKey, value, {});
+					} else if (typeof context.callbacks.main === 'function') {
+						context.callbacks.main('get', element, targetKey, value, {});
 					}
 				}
 			}, delay);
 		},
 
-		// Считывание значения
+		// Read value
 		_readElementValue: function _readElementValue(element, oldValue) {
-			var value = undefined,
-			    elementValue = $(element).attr('value'),
-			    customValue = $(element).attr(this.keys['event-value']);
+			var value = undefined;
 
-			// input:checkbox
-			if (element.is('input[type="checkbox"]') || element.is('input[type="radio"]')) {
-				if (typeof oldValue !== 'boolean' && elementValue !== undefined) {
-					value = $(element).is(':checked') ? elementValue : '';
-				} else {
-					value = $(element).is(':checked');
-				}
+			var elementValue = $(element).attr('value');
+			var customValue = $(element).attr(this.keys['event-value']);
+
+			// data-on-value
+			if (customValue !== undefined && customValue !== '') {
+				value = customValue;
 			}
-			// select
-			else if (element.is('select')) {
-					if (typeof oldValue === 'number') {
-						value = $(element).find(':selected').index();
+			// input:checkbox
+			else if (element.is('input[type="checkbox"]') || element.is('input[type="radio"]')) {
+					if (typeof oldValue !== 'boolean' && elementValue !== undefined) {
+						value = $(element).is(':checked') ? elementValue : '';
 					} else {
-						value = $(element).val();
+						value = $(element).is(':checked');
 					}
 				}
-				// input
-				else if (element.is('input') || element.is('textarea')) {
-						value = $(element).val();
-					}
-					// link
-					else if (element.is('a')) {
-							value = $(element).attr('href');
+				// select
+				else if (element.is('select')) {
+						if (typeof oldValue === 'number') {
+							value = $(element).find(':selected').index();
+						} else {
+							value = $(element).val();
 						}
+					}
+					// input
+					else if (element.is('input') || element.is('textarea')) {
+							value = $(element).val();
+						}
+						// link
+						else if (element.is('a')) {
+								value = $(element).attr('href');
+							}
+							// form
+							else if (element.is('form')) {
+									value = {};
+									$(element[0]).serializeArray().forEach(function (x) {
+										value[x.name] = x.value;
+									});
+								}
 
 			// Если не удалось считать значение
 			if (value === '' || value === undefined) {
-				value = elementValue || customValue || $(element).html();
+				value = elementValue || $(element).html();
 			};
 
 			return value;
 		},
 
-		// Уничтожение плагина
+		// Set value
+		_setElementValue: function _setElementValue(element, event, value) {
+			if (event === 'visible' || event === 'hidden') {
+				var state = event === 'visible' ? 'hidden' : 'visible';
+
+				if (typeof value === 'boolean' && value || typeof value === 'string' && (value === 'yes' || value === 'y' || value === 'true') || typeof value === 'integer' && value >= 1) {
+					state = event;
+				}
+
+				element.css('visibility', state);
+			} else if (event === 'enabled' || event === 'disabled') {
+
+				if (typeof value === 'boolean' && value === true || typeof value === 'string' && (value === 'yes' || value === 'y') || typeof value === 'integer' && value >= 1) {
+					if (event === 'enabled') {
+						element.removeAttr('disabled');
+					} else {
+						element.attr('disabled', 'disabled');
+					}
+				} else {
+					if (event === 'disabled') {
+						element.removeAttr('disabled');
+					} else {
+						element.attr('disabled', 'disabled');
+					}
+				}
+			} else {
+				if (element.is('input') || element.is('textarea')) {
+					element.val(value);
+				} else {
+					element.text(value);
+				}
+			}
+		},
+
+		//
+		// ="focusin,focusout:action" 
+		// ="[click:action1,change:action2]"
+		// ="click:test( '!!!' )"
+		_extractActions: function _extractActions(element, event, actionData, callback) {
+			var defaultEvent = 'click';
+			if (element.is('form')) {
+				defaultEvent = 'submit';
+			} else if (element.is('input, select, textarea')) {
+				defaultEvent = 'change';
+			}
+
+			//
+			var actionList = [];
+
+			//
+			if (typeof actionData !== 'string') {
+				console.error('jQuery.myData: Empty data in [' + context.keys['event'] + '].');
+				return;
+			} else {
+				actionList = actionData.indexOf('[') === 0 ? (actionData.match(/[\[\{}](.*?)[\}\]]/)[1] || '').split(',') : [actionData];
+			}
+
+			//
+			actionList.forEach(function (listItem, i, arr) {
+				var onData = listItem.indexOf(':') >= 0 ? listItem.split(':') : [defaultEvent, listItem];
+				var actions = onData[0].trim().split(',');
+				var value = onData[1].trim();
+
+				// 
+				if (!value) {
+					return;
+				}
+
+				//
+				actions.forEach(function (action, i, arr) {
+					callback(action, value);
+				});
+			});
+		},
+
+		// Destroy
 		destroy: function destroy() {
 			this.unbind();
 		}
 	};
 
-	// Прописываем плагин
+	// Plugin init
 	$.fn[pluginName] = function (params, callback) {
 		var args = arguments;
 
-		// Если параметры это объект
+		// Object
 		if (params === undefined || (typeof params === 'undefined' ? 'undefined' : _typeof(params)) === 'object') {
-			// Проходим по компонентам
 			this.each(function () {
 				var instance = isJQ ? $(this).data('_' + pluginName) : $(this)[0]['_' + pluginName];
 				var id = (typeof params === 'undefined' ? 'undefined' : _typeof(params)) === 'object' && params.hasOwnProperty('exlusive') ? '' : Math.random().toString(36).substring(7);
@@ -375,11 +471,10 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 			return $(this);
 		}
-		// Если параметры это строка
+		// String
 		else if (typeof params === 'string' && params[0] !== '_' && params !== 'init') {
 				var returns = undefined;
 
-				//
 				this.each(function () {
 					var instance = isJQ ? $(this).data('_' + pluginName) : $(this)[0]['_' + pluginName];
 
